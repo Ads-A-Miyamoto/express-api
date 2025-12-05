c1fedc6a46de4491c60f2f048f425fa73c06d839
Your Name <you@example.com>
2025-12-05 13:39:24 +0900

    スタックとレースを実装

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 'use strict';
 
-const debug = require('debug');
-const debugInfo = debug('module:info');
-setInterval(() => {
-  debugInfo('some information.');
-}, 1000);
-const debugError = debug('module:error');
-setInterval(() => {
-  debugError('some error.');
-}, 1000);
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const photosRouter = require('./routes/photos');
 
 const app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
